f0847deaa3fa35273f8aeb62af0d944e97d3139a
Paul Bridi <paulbridi@web011170.wireless.mcgill.ca>
2016-06-27 16:33:57 -0400

    first commit

---
+++ menumaker/script.js
@@ -0,0 +1,16 @@
+(function($){
+$(document).ready(function(){
+
+$("#cssmenu").menumaker({
+  title: "Menu",
+  breakpoint: 768,
+  format: "multitoggle"
+});
+
+$("#cssmenu a").each(function() {
+  var linkTitle = $(this).text();
+  $(this).attr('data-title', linkTitle);
+});
+
+});
+})(jQuery);
